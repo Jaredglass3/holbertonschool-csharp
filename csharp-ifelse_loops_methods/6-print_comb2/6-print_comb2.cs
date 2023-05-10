ba04a5a9a9dac9a639d5f25421f8376f4257d008
Jaredglass3 <Jaredglass352@gmail.com>
2023-05-10 11:42:58 -0500

    Made project for task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,29 @@
+﻿﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int j = 0;
+            for (i = 0; i <= 8; i++)
+            {
+                while (j <= 9)
+                {
+                    if (i == 8 && j == 9)
+                    {
+                        Console.WriteLine("89");
+                    }
+                    else if (i != j)
+                    {
+                        Console.Write("{0}{1}, ", i, j);
+                    }
+                    j++;
+                }
+                j = i + 1;
+            }
+        }
+    }
+}
