a6a90a2b9de4d9d3df9b55f2ef21b0e836adb0b2
Jaredglass3 <Jaredglass352@gmail.com>
2023-05-11 09:23:29 -0500

    Made project for task 0

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,28 @@
+﻿﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+            Console.WriteLine();
+
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] new_array = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            new_array[i] = i;
+            if (i != size - 1)
+                Console.Write(i + " ");
+            else
+                Console.WriteLine(i);
+        }
+        return new_array;
+    }
+} 
