c931dab92a342dd63577c855e5bba1e01f664e91
Jaredglass3 <Jaredglass352@gmail.com>
2023-05-24 12:04:56 -0500

    Task 0

---
+++ csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,14 @@
+﻿﻿using System;
+
+/// <sumary> contains multiple method for objects </sumary>
+class Obj
+{
+    /// <sumary> check if an object is an int or no </sumary>
+    /// <return> true if <paramref name="obj"/> is int </return>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj is int)
+            return true;
+        return false;
+    }
+}
