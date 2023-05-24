86c9e94ae57dde1a8fbe4e284018aaf89e9a1fc7
Jaredglass3 <Jaredglass352@gmail.com>
2023-05-24 12:12:10 -0500

    Task 2

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <sumary> contains multiple method for objects </sumary>
+class Obj
+{
+    /// <sumary> check if an object <paramref name="derivedType"/> is a subclass of <paramref name="baseType"/></sumary>
+    /// <return> true if <paramref name="derivedType"/> is a subclass of <paramref name="baseType"/></return>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
