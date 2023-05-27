f71b73af26e06a36277c9c5675561af84776ff18
Jaredglass3 <Jaredglass352@gmail.com>
2023-05-27 16:20:09 -0500

    Task 6

---
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,36 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// class that cointain diferent method to handle vectors
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="vector1"></param>
+    /// <param name="vector2"></param>
+    /// <returns></returns>
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        double[] fail = new double[] {-1}; 
+        if (vector1.Length != vector2.Length)
+            return fail;
+
+
+        if (vector1.Length == 2 || vector1.Length == 3)
+        {
+            double[] new_vect = new double[vector1.Length];
+
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                new_vect[i] = vector1[i] + vector2[i];
+            }
+
+            return (new_vect);
+        }
+
+        return (fail);
+    }
+}
