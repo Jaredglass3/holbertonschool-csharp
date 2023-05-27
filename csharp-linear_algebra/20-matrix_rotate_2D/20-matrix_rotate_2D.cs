4677cc3183b49a1399d87dc315cd3fe40937aefc
Jaredglass3 <Jaredglass352@gmail.com>
2023-05-27 17:19:09 -0500

    Attempted corrections

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -10,38 +10,46 @@ class MatrixMath
     /// </summary>
     /// <param name="matrix1">Matrix 1</param>
     /// <param name="matrix2">Matrix 2</param>
-    /// <returns>returns the new matrix</returns>
-    public static double[,] Multiply(double[,] matrix1, double[,] matrix2){
+    /// <returns>Returns the new matrix</returns>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
         if (matrix1.Length == 0 ||
             matrix2.Length == 0 ||
-            matrix1.GetLength(1) != matrix2.GetLength(0)){
-            return (new double[,]{{-1}});
+            matrix1.GetLength(1) != matrix2.GetLength(0))
+        {
+            return new double[,] { { -1 } };
         }
-        var res = new double[matrix1.GetLength(0),matrix2.GetLength(1)];
-        double sum = 0.0;
-        for (int y = 0; y < matrix1.GetLength(0); y++){
-            for (int x2 = 0; x2 < matrix2.GetLength(1); x2++){
-                sum = 0;
-                for (int x = 0; x < matrix1.GetLength(1); x++){
-                    sum = Math.Round(sum + (matrix1[y,x] * matrix2[x, x2]), 2);
+
+        var res = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        for (int y = 0; y < matrix1.GetLength(0); y++)
+        {
+            for (int x2 = 0; x2 < matrix2.GetLength(1); x2++)
+            {
+                double sum = 0;
+                for (int x = 0; x < matrix1.GetLength(1); x++)
+                {
+                    sum += matrix1[y, x] * matrix2[x, x2];
                 }
                 res[y, x2] = Math.Round(sum, 2);
             }
         }
-        return res;  
+
+        return res;
     }
+
     /// <summary>
     /// Method that rotates a square 2D matrix by a given angle in radians
     /// </summary>
     /// <param name="matrix">Matrix to rotate</param>
     /// <param name="angle">New angle</param>
-    /// <returns>returns the resulting matrix.</returns>
-    public static double[,] Rotate2D(double[,] matrix, double angle){
-        double cosAngle = System.Math.Cos(angle);
-        double sinAngle = System.Math.Sin(angle);
-        var rotation = new double[2,2]{{cosAngle, sinAngle}, {-sinAngle, cosAngle}};
-        var res = new double[matrix.GetLength(0),matrix.GetLength(1)];
-        res = Multiply(matrix, rotation);
+    /// <returns>Returns the resulting matrix.</returns>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double cosAngle = Math.Cos(angle);
+        double sinAngle = Math.Sin(angle);
+        var rotation = new double[2, 2] { { cosAngle, sinAngle }, { -sinAngle, cosAngle } };
+        var res = Multiply(matrix, rotation);
         return res;
     }
 }
