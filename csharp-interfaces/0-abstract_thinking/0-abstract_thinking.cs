49a752a44f615b7d10507bbe7febcc70b8606b40
Jaredglass3 <Jaredglass352@gmail.com>
2023-05-30 09:23:13 -0500

    Task 0

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// abstract class to define a name and override ToString() method
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// name of the object, default is null
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// TosString() override method
+    /// </summary>
+    /// <returns>'name' is a 'type'</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType()}";
+    }
+}
